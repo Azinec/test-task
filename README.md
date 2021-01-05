b97f53bbcb25d45ab31c46955ae9f3a203663f71
Yuri Kulyk <katana_2w@ukr.net>
2021-01-06 00:28:04 +0400

    Changes: - README update - controllers/monthlyRateCalculate fix

--- README.md
+++ README.md
@@ -15,21 +15,21 @@ In browser reach url - http://localhost:3000/
 - check result
 
 Params:
-- Amount (Main amount) - based amount for calculation
-- Down payment - amount that subtract from Main amount before calculation
-- Number of months - number of months for calculation monthly payment
-- Interested rate - rate for the year (should be transferred for monthly use)
-- Final payment (optional) - amount that should stay after all monthly payments
+Amount (Main amount) - initial amount for calculation
+Down payment - amount we subtract from Main amount before calculation
+Number of months - number of months for calculation monthly rate
+Interested rate - rate for the year (should be transferred for monthly use)
+Final payment (optional) - amount that should stay after all monthly payments
 
 Notes:
 - all cases you find in tests "tests/calculation.unit.test.js"
 
 ### Task 2. Gross to Net calculation
 You can use 2 ways for checking:
-- you can use terminal: firstly, go to file tasks/vatGrossNet.js and uncommented code based on a hint. After that got to terminal and run ``` node tasks/vatGrossNet.js ```
+- you can use terminal: firstly, go to file tasks/vatGrossNet.js and uncomment code based on a hint. After that got to terminal and run ``` node tasks/vatGrossNet.js ```
 - you can check how it works with test in "tests/grossToNet.unit.test.js"
 
 ### Task 3. Custom flat method for array
 You can use 2 ways for checking:
-- you can use terminal: firstly, go to file tasks/flatCustom.js and uncommented code based on a hint. After that got to terminal and run ``` node tasks/flatCustom.js ```
+- you can use terminal: firstly, go to file tasks/flatCustom.js and uncomment code based on a hint. After that got to terminal and run ``` node tasks/flatCustom.js ```
 - you can check how it works with test in "tests/flatCustom.unit.test.js"
--- controllers/monthlyRateCalculate.js
+++ controllers/monthlyRateCalculate.js
@@ -4,6 +4,21 @@
 const { monthlyPayment } = require('../services/monthlyRateCalculate');
 
 /**
+ * should be moved in helpers or other path based on project structure
+ * @param parametersObject
+ * @returns {{convertedNumMonths: number, convertedDownPayment: number, convertedMainAmount: number, convertedRate: number, convertedFinalPayment: number}}
+ */
+const convertParameters = (parametersObject) => {
+    const { mainAmount, downPayment, numMonths, rate, finalPayment = 0 } = parametersObject;
+    const convertedMainAmount = Number.isNaN(parseFloat(mainAmount)) ? undefined : parseFloat(mainAmount);
+    const convertedDownPayment = Number.isNaN(parseFloat(downPayment)) ? undefined : parseFloat(downPayment);
+    const convertedNumMonths = Number.isNaN(parseFloat(numMonths)) ? undefined : parseFloat(numMonths);
+    const convertedRate = Number.isNaN(parseFloat(rate)) ? undefined : parseFloat(rate);
+    const convertedFinalPayment = Number.isNaN(parseFloat(finalPayment)) ? undefined : parseFloat(finalPayment);
+    return { convertedMainAmount, convertedDownPayment, convertedNumMonths, convertedRate, convertedFinalPayment };
+}
+
+/**
  * calculateMonthlyRate - simple controller method - all logic in monthlyRateCalculate service
  *
  * @param req
@@ -11,9 +26,8 @@ const { monthlyPayment } = require('../services/monthlyRateCalculate');
  * @returns {Promise<*>}
  */
 const calculateMonthlyRate = async (req, res) => {
-    const { mainAmount, downPayment, numMonths, rate, finalPayment = 0 } = req.body;
-
-    const resultCalculation = monthlyPayment(mainAmount, downPayment, numMonths, rate, finalPayment);
+    const { convertedMainAmount, convertedDownPayment, convertedNumMonths, convertedRate, convertedFinalPayment } = convertParameters(req.body);
+    const resultCalculation = monthlyPayment(convertedMainAmount, convertedDownPayment, convertedNumMonths, convertedRate, convertedFinalPayment);
     if(resultCalculation.errors.length > 0) {
         return res.status(400).send({message: 'Failed', errors: resultCalculation.errors});
     }
